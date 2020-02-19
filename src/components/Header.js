1851f9a9525a8d4908d0490de7456fcc839dc7a8
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-02-19 16:34:24 -0500

    header structured

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <div className="header-container">
+            <img src="https://lh3.googleusercontent.com/proxy/bxNVDJRfT5Tqn_xfWTEBHCwrbhVIYZpyqg4-GHwXFFHmAWRC33WxIoLzz20m5I69CMpSL_c_x-yEOs17NSeAaiZeAdcWrP4" alt ="nasa-logo"></img>
+            <h1 className="page-title">Nasa Photo of the Day</h1>
+        </div>
+    )
+}
