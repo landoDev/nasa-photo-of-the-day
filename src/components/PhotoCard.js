075d79f10e8332450199e555c76fb97f8033645b
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-02-19 16:52:13 -0500

    added axios dependency. stareted to code and make request

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,3 +1,24 @@
 import React, {useState, useEffect} from "react";
 import Photo from "./Photo";
 import InfoCard from "./InfoCard";
+import axios from "axios";
+
+export default function PhotoCard (){
+    const [data, setData] = useState([])
+
+    useEffect(()=>{
+        axios.get("https://api.nasa.gov/planetary/apod")
+        .then(response => {
+            console.log(response);
+            // setData(response)
+        })
+        .catch(error => {
+            console.log("Houston, we have a problem: ", error)
+        })
+    })
+    // return(
+    //     <Photo />
+    //     <InfoCard />
+    // );
+
+}
