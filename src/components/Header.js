cee42eeb94e0356b1e7a2b5dbe30eec388fe413a
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-02-19 18:09:43 -0500

    capitilized NASA

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header () {
     return (
         <div className="header-container">
             <img src="https://lh3.googleusercontent.com/proxy/bxNVDJRfT5Tqn_xfWTEBHCwrbhVIYZpyqg4-GHwXFFHmAWRC33WxIoLzz20m5I69CMpSL_c_x-yEOs17NSeAaiZeAdcWrP4" alt ="nasa-logo"></img>
-            <h1 className="page-title">Nasa Photo of the Day</h1>
+            <h1 className="page-title">NASA Photo of the Day</h1>
         </div>
     )
 }
